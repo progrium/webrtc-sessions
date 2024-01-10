69abad020f5592e9e89b9b96a9b9e56384b0f832
Matt Good <29016+mgood@users.noreply.github.com>
2024-01-10 09:21:56 -0800

    fix track test by padding audio length

--- span/span_test.go
+++ span/span_test.go
@@ -19,24 +19,39 @@ var eqopts = cmp.Options{
 }
 
 func TestTrack(t *testing.T) {
+	rate := beep.SampleRate(48000)
 	track := &Track{
 		start: 0,
+		audio: beep.NewBuffer(beep.Format{
+			SampleRate:  rate,
+			NumChannels: 2,
+			Precision:   2,
+		}),
 	}
+	track.AddAudio(generators.Silence(rate.N(10 * time.Millisecond)))
 	assert.DeepEqual(t, []Annotation(nil), track.Annotations("foo"))
 
 	track.Annotate("foo", "foo-one")
 	types := track.AnnotationTypes()
 	assert.DeepEqual(t, []string{"foo"}, types)
 
-	assert.DeepEqual(t, []Annotation{
-		{Start: 0, End: Timestamp(10 * time.Millisecond), Type: "foo", Data: "foo-one"},
-	}, track.Annotations("foo"), cmpopts.IgnoreFields(Annotation{}, "ID", "track"))
+	assert.DeepEqual(t,
+		[]Annotation{
+			{Start: 0, End: Timestamp(10 * time.Millisecond), Type: "foo", Data: "foo-one"},
+		},
+		track.Annotations("foo"),
+		eqopts, cmpopts.IgnoreFields(Track{}, "ID"), cmpopts.IgnoreFields(Annotation{}, "ID"),
+	)
 
 	track.Span(Timestamp(5*time.Millisecond), Timestamp(10*time.Millisecond)).Annotate("foo", "foo-two")
-	assert.DeepEqual(t, []Annotation{
-		{Start: 0, End: Timestamp(10 * time.Millisecond), Type: "foo", Data: "foo-one"},
-		{Start: Timestamp(5 * time.Millisecond), End: Timestamp(10 * time.Millisecond), Type: "foo", Data: "foo-two"},
-	}, eqopts)
+	assert.DeepEqual(t,
+		[]Annotation{
+			{Start: 0, End: Timestamp(10 * time.Millisecond), Type: "foo", Data: "foo-one"},
+			{Start: Timestamp(5 * time.Millisecond), End: Timestamp(10 * time.Millisecond), Type: "foo", Data: "foo-two"},
+		},
+		track.Annotations("foo"),
+		eqopts, cmpopts.IgnoreFields(Track{}, "ID"), cmpopts.IgnoreFields(Annotation{}, "ID"),
+	)
 }
 
 func TestSerializeTrack(t *testing.T) {
