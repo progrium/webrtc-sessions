8d9f8973b1d47d4cf90042909d35de4754b35dd0
Jeff Lindsay <progrium@gmail.com>
2024-01-11 14:31:06 -0800

    add audio toggle for muting

--- web/localmedia.js
+++ web/localmedia.js
@@ -4,8 +4,10 @@ class LocalMedia {
     this.ondevicechange = () => null;
     this.audioDevices = [];
     this.videoDevices = [];
+    this.outputDevices = [];
     this.audioSource = undefined;
     this.videoSource = undefined;
+    this.audioEnabled = true;
     this.stream = undefined;
     this.updateStream();
     this.updateDevices();
@@ -19,6 +21,11 @@ class LocalMedia {
 
   setVideoSource(deviceId) {
     this.videoSource = deviceId;
+    this.updateStream();
+  }
+
+  toggleAudio() {
+    this.audioEnabled = !this.audioEnabled;
     this.updateStream();
   }
 
@@ -39,7 +46,10 @@ class LocalMedia {
         audio: {deviceId: this.audioSource ? {exact: this.audioSource} : true},
         video: {deviceId: this.videoSource ? {exact: this.videoSource} : true}
       });
-   }
+    }
+    if (!this.audioEnabled) {
+      this.stream.getAudioTracks()[0].enabled = false;
+    }
     if (this.onstreamchange) {
       this.onstreamchange(this.stream);
     }
@@ -49,6 +59,7 @@ class LocalMedia {
     const devices = await navigator.mediaDevices.enumerateDevices();
     this.audioDevices = devices.filter(({kind}) => kind === "audioinput");
     this.videoDevices = devices.filter(({kind}) => kind === "videoinput");
+    this.outputDevices = devices.filter(({kind}) => kind === "audiooutput");
     if (this.ondevicechange) {
       this.ondevicechange();
     }
